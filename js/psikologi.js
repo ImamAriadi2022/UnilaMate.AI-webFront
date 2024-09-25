c290d391f11f7d2410ad69a2430d7b5e99216f41
Imam ariadi <gamerapin139@gmail.com>
2024-09-26 00:41:04 +0700

    logika deteksi kepribadian dan benerin typo

--- js/psikologi.js
+++ js/psikologi.js
@@ -1,56 +1,45 @@
+function testPsikologi(event){
+    event.preventDefault(); // Mencegah halaman refresh
+    console.log("submit berhasil");
 
-
-function testPsikologi(){
-    console.log("submit berhasil")
     // Mengambil semua elemen input yang dipilih dari setiap pertanyaan
     const form = document.forms['quizForm'];
     let totalScore = 0;
     let psikologi = document.getElementById("psikologi-div");
 
-    if (psikologi.style.display == "block"){
-        psikologi.style.display = "none";
-     } else {
-        psikologi.style.display = "block";
-     }
-
-
-    
     // Loop untuk menghitung nilai setiap jawaban
     for (let i = 1; i <= 13; i++) {
         const selected = form[`q${i}`].value;
         if (selected) {
             totalScore += parseInt(selected);
+            console.log(totalScore);
         }
     }
-    
+    console.log(totalScore);
     // Memutuskan apakah introvert atau ekstrovert berdasarkan skor
     let resultText = "";
-    if (totalScore >= 36 && totalScore <= 60) {
+    if (totalScore >= 36 && totalScore <= 100) {
         resultText = "Anda cenderung ekstrovert.";
+        psikologi.style.display = "block";
     } else if (totalScore >= 12 && totalScore <= 35) {
         resultText = "Anda cenderung introvert.";
+        psikologi.style.display = "block";
     } else {
         resultText = "Silakan isi semua pertanyaan.";
         alert("tolong isi form terlebih dahulu");
         psikologi.style.display = "none";
     }
     
-        // Menampilkan hasil di halaman
-        document.getElementById('result').innerText = resultText;
+    // Menampilkan hasil di halaman
+    document.getElementById('result').innerText = resultText;
 }
 
 
 function closePopUpAllert(){
-    let psikologi = document.getElementById("psikologi-div");
-    if (psikologi.style.display == "block"){
+    const psikologi = document.getElementById('psikologi-div')
+    if (psikologi.style.display === "block"){
         psikologi.style.display = "none";
-     } else {
+    } else{
         psikologi.style.display = "block";
-     }
+    }
 }
-
-
-// fungsi menampilkan hasil 
-// function calculateScore() {
-
-// }
