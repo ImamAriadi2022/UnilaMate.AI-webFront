961111e9c492321b8ebb66bb3a6642fbc284fb93
Imam ariadi <gamerapin139@gmail.com>
2024-09-25 17:52:07 +0700

    fungsi psikologi dah keluar pop up cuma belum sempurna

--- js/psikologi.js
+++ js/psikologi.js
@@ -2,4 +2,11 @@
 
 function testPsikologi(){
     console.log("submit berhasil")
+    
+    let psikologi = document.getElementById("psikologi-div");
+    if (psikologi.style.display == "block"){
+        psikologi.style.display = "none";
+     } else {
+        psikologi.style.display = "block";
+     }
 }
